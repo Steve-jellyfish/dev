114e054c9b9840a64e43179d69afb64f4aad70da
Di Wu <di.wu@fvfyp362hv2h.jellyfish.office>
2019-06-17 17:35:18 -0400

    updating goal.sql

--- uipath/goal.sql
+++ uipath/goal.sql
@@ -3,7 +3,39 @@
 add comments here
 */
 
-with session as (
+
+with hit as (
+SELECT 
+	fullVisitorId,
+	visitId,
+	h.hitNumber,
+	any_value(case when (h.page.pagePath = 'www.uipath.com/g/thank-you-install' 
+						and h.isInteraction = True) then 1 else 0 end) as goals_successful_installation,
+	any_value(case when (h.page.pagePath = 'www.uipath.com/g/thank-you-activating' 
+						and h.isInteraction = True) then 1 else 0 end) as goals_successful_activation,	
+	any_value(case when (h.eventInfo.eventCategory = 'Lead' 
+					and h.eventInfo.eventAction in ('New', 'Change')
+					and h.eventInfo.eventLabel = 'Qualified') then 1 else 0 end) as goals_sdfc_lead_qualified
+from `marketing-224613.69782380.ga_sessions_*`, UNNEST(hits) as h
+where _TABLE_SUFFIX between '20190610' and '20190616'
+group by 1,2,3
+)
+
+select 
+	fullVisitorId as full_visitor_id,
+	visitId as visit_id,
+	sum(goals_successful_installation) as goals_successful_installation,
+	sum(goals_successful_activation) as goals_successful_activation,
+	sum(goals_sdfc_lead_qualified) as goals_sdfc_lead_qualified
+from hit
+group by 1, 2
+
+
+/* for validation 
+see GA report here:
+https://analytics.google.com/analytics/web/template?uid=OE9KjgznQ5Kybf2gMiyksw
+
+with hit as (
 SELECT 
 	fullVisitorId,
 	visitId,
@@ -26,8 +58,9 @@ SELECT
 	sum(goals_successful_installation) as goals_successful_installation,
 	sum(goals_successful_activation) as goals_successful_activation,
 	sum(goals_sdfc_lead_qualified) as goals_sdfc_lead_qualified
-from session
+from hit
 group by 1
 order by 1
 
+*/
 
